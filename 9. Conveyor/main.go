d5c156e5cf17fda9f401e434638c3c06c3955fc9
Valeriya Subocheva <subocheva.valeria@gmail.com>
2022-09-10 14:02:52 +0300

    9

--- 9. Conveyor/main.go
+++ 9. Conveyor/main.go
@@ -8,11 +8,23 @@ import (
 // 9.	Разработать конвейер чисел. Даны два канала: в первый пишутся числа (x) из массива, во второй — результат операции x*2, после чего данные из второго канала должны выводиться в stdout.
 
 func main() {
-	var
+	data := []int{1, 4, 3}
+	var dataResultForTest []int
+	chIn := make(chan int, len(data))
+	chOut := make(chan int, len(data))
+	wg := sync.WaitGroup{}
 
+	wg.Add(len(data))
+	go readFromSlice(data, chIn)
+	go multiplicate(chIn, chOut)
+	go toOutput(chOut, &wg, dataResultForTest)
+
+	fmt.Println(dataResultForTest)
+
+	wg.Wait() // wait until all goroutines will do their job
 }
 
-// read data from slice
+// Read data from slice
 func readFromSlice(data []int, ch chan int) {
 	defer close(ch)
 	for _, elem := range data {
@@ -20,18 +32,23 @@ func readFromSlice(data []int, ch chan int) {
 	}
 }
 
-// read from first slice to second and do
-func multi2(chIn chan int, chOut chan int) {
+// Read from first slice to second and do operation x*2
+func multiplicate(chIn chan int, chOut chan int) {
 	defer close(chOut)
 	for elem := range chIn {
 		chOut <- elem * 2
 	}
 }
 
-//
-func readOutput(ch chan int, wg *sync.WaitGroup) {
+// Output in stdout
+func toOutput(ch chan int, wg *sync.WaitGroup) {
+	var mu sync.Mutex
 	for elem := range ch {
 		fmt.Println(elem)
-		wg.Done() //
+		mu.Lock()
+		dataResultForTest := append(dataResultForTest, elem)
+		fmt.Println(dataResultForTest)
+		mu.Unlock()
+		wg.Done()
 	}
 }
