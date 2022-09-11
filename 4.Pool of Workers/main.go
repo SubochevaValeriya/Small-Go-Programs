f73e30110cc9596a1e34031291af9344caba5d1c
Valeriya Subocheva <subocheva.valeria@gmail.com>
2022-09-11 20:17:54 +0300

    4.PullOfWorkers

--- 4.Pool of Workers/main.go
+++ 4.Pool of Workers/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // 4.	Реализовать постоянную запись данных в канал (главный поток).
 //Реализовать набор из N воркеров, которые читают произвольные данные из канала
@@ -8,29 +11,47 @@ import "fmt"
 //
 //Программа должна завершаться по нажатию Ctrl+C. Выбрать и обосновать способ завершения работы всех воркеров.
 
-func main() {
-	data := []any{1, 2, 3}
-
-	ch := make(chan any, len(data))
-
-	for i := 0; i < len(data); i++ {
-		ch <- data[i]
+// Here's the worker, of which we'll run several
+// concurrent instances. These workers will receive
+// work on the `jobs` channel and send the corresponding
+// results on `results`. We'll sleep a second per job to
+// simulate an expensive task.
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
 	}
-
-	fmt.Println(workers(len(data), ch))
 }
 
-func workers(n int, ch <-chan any) []any {
-	data := []any{}
-	workers := make(chan struct{}, 1)
-	for i := 1; i <= n; i++ {
-		fmt.Println("h")
-		go func() {
-			fmt.Println("h")
-			fmt.Println(ch)
-			data = append(data, ch)
-			workers <- struct{}{}
-		}()
+func main() {
+
+	// In order to use our pool of workers we need to send
+	// them work and collect their results. We make 2
+	// channels for this.
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	// This starts up 3 workers, initially blocked
+	// because there are no jobs yet.
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
 	}
-	return data
+
+	// Here we send 5 `jobs` and then `close` that
+	// channel to indicate that's all the work we have.
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Finally we collect all the results of the work.
+	// This also ensures that the worker goroutines have
+	// finished. An alternative way to wait for multiple
+	// goroutines is to use a [WaitGroup](waitgroups).
+	for a := 1; a <= numJobs; a++ {
+		<-results
+	}
 }
